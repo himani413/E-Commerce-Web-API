46922b78340438d9c06b665f6ec5a84866af2cb9
Himani Perera <pererau-se19042@stu.kln.ac.lk>
2023-08-15 00:14:55 +0530

    feat: improve file structure

--- app.js
+++ app.js
@@ -1,70 +1,46 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
-
+const cors = require('cors');
 require('dotenv/config');
-const api = process.env.API_URL;
 
 
-//middleware
-app.use(bodyParser.json());
+app.use(cors());
+app.options('*', cors());
+
+// Middleware
+app.use(express.json());
 app.use(morgan('tiny'));
 
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock:{
-        type: Number,
-        required: true
-    }
-})
+//Routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
 
-const Product = mongoose.model('Product', productSchema);
+const api = process.env.API_URL;
 
-app.get(`${api}/products`, async(req, res) => {    
-    const productList = await Product.find();
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
 
-    if(!productList) {
-        res.status(500).json({success: false})
-    }
-    res.send(productList);
-});
-
-app.post(`${api}/products`, (req, res) => {    
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
-
-    product.save().then((createdProduct => {
-        res.status(201).json(createdProduct)
-    })).catch((err) => {
-        console.log(err);
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
-});
-
+//Database
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     dbName: 'eshop-database'
 })
-.then(() => {
-    console.log('Database Connection is ready...');
+.then(()=>{
+    console.log('Database Connection is ready...')
 })
-.catch((err) => {
+.catch((err)=> {
     console.log(err);
 })
 
-app.listen(3000, () => {
-    
-    console.log(api);
-    console.log('Server is running http://localhost:3000');
-});
+//Server
+app.listen(3000, ()=>{
+
+    console.log('server is running http://localhost:3000');
+})
