9fddb59a32fe22878b49737cd59bb5d0023f099c
Himani Perera <pererau-se19042@stu.kln.ac.lk>
2023-08-14 17:33:46 +0530

    feat: update dependencies

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+
+//middleware
+app.use(bodyParser.json());
 
 require('dotenv/config');
 
 const api = process.env.API_URL;
 
-http://localhost:3000/api/v1/products
+app.get(`${api}/products`, (req, res) => {    
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some url',
+    };
+    res.send(product);
+});
 
-app.get('/', (req, res) => {    
-    res.send('Hello API');
+app.post(`${api}/products`, (req, res) => {    
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct);
 });
 
 app.listen(3000, () => {
+    
     console.log(api);
-  console.log('Server is running http://localhost:3000');
+    console.log('Server is running http://localhost:3000');
 });
