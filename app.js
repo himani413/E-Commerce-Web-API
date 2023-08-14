bd3fef205d2c3e07ac067629f4ff78ad7ee22ba4
Himani Perera <pererau-se19042@stu.kln.ac.lk>
2023-08-14 21:00:16 +0530

    feat: create APIs, make connection with MongoDB

--- app.js
+++ app.js
@@ -1,29 +1,67 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+
+require('dotenv/config');
+const api = process.env.API_URL;
 
 
 //middleware
 app.use(bodyParser.json());
+app.use(morgan('tiny'));
 
-require('dotenv/config');
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock:{
+        type: Number,
+        required: true
+    }
+})
 
-const api = process.env.API_URL;
+const Product = mongoose.model('Product', productSchema);
 
-app.get(`${api}/products`, (req, res) => {    
-    const product = {
-        id: 1,
-        name: 'hair dresser',
-        image: 'some url',
-    };
-    res.send(product);
+app.get(`${api}/products`, async(req, res) => {    
+    const productList = await Product.find();
+
+    if(!productList) {
+        res.status(500).json({success: false})
+    }
+    res.send(productList);
 });
 
 app.post(`${api}/products`, (req, res) => {    
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct);
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    })
+
+    product.save().then((createdProduct => {
+        res.status(201).json(createdProduct)
+    })).catch((err) => {
+        console.log(err);
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    })
 });
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'eshop-database'
+})
+.then(() => {
+    console.log('Database Connection is ready...');
+})
+.catch((err) => {
+    console.log(err);
+})
 
 app.listen(3000, () => {
     
