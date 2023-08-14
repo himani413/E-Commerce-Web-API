b09c5cc9f3304abee25f2c92e5b6684fd0104ead
Himani Perera <pererau-se19042@stu.kln.ac.lk>
2023-08-14 16:50:55 +0530

    feat: initialize dependencies

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
+http://localhost:3000/api/v1/products
+
+app.get('/', (req, res) => {    
+    res.send('Hello API');
+});
+
+app.listen(3000, () => {
+    console.log(api);
+  console.log('Server is running http://localhost:3000');
+});
